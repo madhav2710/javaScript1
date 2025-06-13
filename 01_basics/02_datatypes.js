7a8d67118d2ea20b8d9120ac3576810ec8e3a3db
madhav2710 <madhavjavia777@gmail.com>
2025-06-13 19:58:23 +0530

    Basic Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,5 @@
+// null is an standalone value that represents the absence of any object value.
+// undifiened is a type that represents a variable that has been declared but not assigned a value.
+// symbol is a unique and immutable primitive value that can be used as an identifier for object properties.
+console.log(typeof null); // "object"
+console.log(typeof undefined); // "undefined";
