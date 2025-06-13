f4b448202a92edc549c932b4689f379f3a968e6c
madhav2710 <madhavjavia777@gmail.com>
2025-06-13 23:12:05 +0530

    updated conversionOperation

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -9,3 +9,24 @@ console.log(ScoreStringtoNumber);//it will Show NaN (Not a Number) because "100a
 console.log(ScoreStringtoBoolean);
 console.log(typeof ScoreStringtoBoolean);
 // when there is "" as input then it will be false in case of boolean conversion;
+// *****Operations*****
+let value=3;
+let negativeValue=-value;
+console.log(negativeValue); // -3
+console.log(2**3);//2pwr3;
+let str1="100";
+let str2="200";
+let str3=str1+str2; // Concatenation;
+console.log(str3); // "100200"
+
+
+// --------
+console.log("1"+ 2 + 3);
+// "123" because the first operation is string concatenation;
+console.log(1 + 2 + "3");
+// "33" because the first two operations are numeric addition;
+console.log(+true); // 1
+// console.log(true+); // 0error
+
+
+
