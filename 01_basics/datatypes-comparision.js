36f0f22fb6a4be00cd616c7d9cbced62a62022bf
madhav2710 <madhavjavia777@gmail.com>
2025-06-14 12:22:13 +0530

    Stackandheap

--- 01_basics/datatypes-comparision.js
+++ 01_basics/datatypes-comparision.js
@@ -14,3 +14,29 @@ let obj={
 const myFunction = function() {
     console.log("Hello, World!");
 }
+// ******;
+
+// Stack memory and Heap memory
+// Stack memory is used for primitive types 
+// and function calls
+// Heap memory is used for reference types 
+// like objects, arrays, and functions
+
+let user1="m@empl.com";
+let user2= user1;
+user2="jp@exmpl.com";
+console.log(user1); //
+console.log(user2); //
+// here as user1 is a primitive type,
+// it is stored in stack memory and user2 is a copy of user1
+// so changing user2 does not affect user1
+
+let user3={
+    name:"mj",
+    email:"m@jmpl.com"
+};
+let user4=user3;
+user4.email="js@gml.com";
+console.log(user3.email); //// here user3 is a reference type,
+// so user4 is a reference to user3
+// changing user4 affects user3
